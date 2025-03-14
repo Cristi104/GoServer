8618d4fcfd99ff85996df6b143d6dc7dc7b20a3c
Cristi104 <petrecristian2004@gmail.com>
2025-03-14 15:34:22 +0200

    added sql table creation script

---
+++ sql/create_tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(64),
+    email VARCHAR(128) UNIQUE,
+    password_hash VARCHAR(256)
+);
+
+CREATE TABLE messages (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    send_date DATETIME,
+    body VARCHAR(256),
+    sender_id INTEGER,
+    receiver_id INTEGER,
+    FOREIGN KEY(sender_id) REFERENCES users(id) ON DELETE RESTRICT,
+    FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE RESTRICT
+);
