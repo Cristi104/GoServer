511844d96c8511f8ccd605d47a08a44f3fe8cc78
Cristi104 <petrecristian2004@gmail.com>
2025-03-25 12:38:28 +0200

    refactored database schema so that it can acomodate the app functionality better

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,8 +1,21 @@
 CREATE TABLE users (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(64),
+    username VARCHAR(64) UNIQUE,
     email VARCHAR(128) UNIQUE,
     password VARCHAR(256)
+);
+
+CREATE TABLE conversations (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    create_date DATETIME NOT NULL
+);
+
+CREATE TABLE in_conversation (
+    user_id INTEGER,
+    conversation_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE RESTRICT,
+    FOREIGN KEY(conversation_id) REFERENCES conversations(id) ON DELETE CASCADE
 );
 
 CREATE TABLE messages (
@@ -10,7 +23,7 @@ CREATE TABLE messages (
     send_date DATETIME,
     body VARCHAR(256),
     sender_id INTEGER,
-    receiver_id INTEGER,
+    conversation_id INTEGER,
     FOREIGN KEY(sender_id) REFERENCES users(id) ON DELETE RESTRICT,
-    FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE RESTRICT
+    FOREIGN KEY(conversation_id) REFERENCES conversations(id) ON DELETE CASCADE
 );
