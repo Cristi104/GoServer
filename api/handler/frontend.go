f5601fbcfd269b48030e6b405437af057c40c2c1
Cristi104 <petrecristian2004@gmail.com>
2025-07-06 17:03:23 +0300

    added serving index on 404 in order for react router to function properly

--- api/handler/frontend.go
+++ api/handler/frontend.go
@@ -2,9 +2,26 @@ package handler
 
 import (
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
+func isDirectory(path string) bool {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return fi.IsDir()
+}
+
 func FrontendHandler(w http.ResponseWriter, r *http.Request) {
+	buildDir := "./frontend/dist"
+	path := filepath.Join(buildDir, r.URL.Path)
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) || isDirectory(path) {
+		http.ServeFile(w, r, filepath.Join(buildDir, "index.html"))
+		return
+	}
 	fs := http.FileServer(http.Dir("frontend/dist/"))
 	fs.ServeHTTP(w, r)
 }
